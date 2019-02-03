649c4438b3123ab9f776827d3b011a63382a15bd
Jeremy de Oliveira-Kumar <eremyj@gmail.com>
2019-02-03 16:59:40 +1100

    Updates made to dynamically import jquery instead of hard require

--- src/jquery.big-slide.js
+++ src/jquery.big-slide.js
@@ -5,7 +5,7 @@
     define(['jquery'], factory);
   } else if (typeof exports === 'object') {
     // Node/CommonJS
-    module.exports = factory(require('jquery'));
+    module.exports = (await import(/* webpackChunkName: "jquery" */ /* webpackMode: "lazy" */ 'jquery')).default;
   } else {
     // Browser globals
     factory(jQuery);
